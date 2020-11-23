e59913fcf2d8d0e9a6b4c5060305d5177567cc78
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-11-23 08:36:16 +0100

    Added Bookstore dbcontext

---
+++ BookStore.Data/BookStoreDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BookStore.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookStore.Data
+{
+    public class BookStoreDbContext : DbContext
+    {
+        public BookStoreDbContext(DbContextOptionsBuilder builder)
+            : base(builder.Options)
+        {
+        }
+
+        public DbSet<Author> Authors { get; set; }
+        public DbSet<Book> Books { get; set; }
+    }
+}
