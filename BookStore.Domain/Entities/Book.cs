f84edb0eb8f11c878e09aa5562ba86f38185c9b5
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-11-23 08:35:22 +0100

    Created basic entities: Author and Book

---
+++ BookStore.Domain/Entities/Book.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BookStore.Domain.Entities
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        public Guid AuthorId { get; set; }
+        public virtual Author Author { get; set; }
+        public string ImageUrl { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string Isbn { get; set; }
+
+        [Required]
+        public double? Price { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+        public string Summary { get; set; }
+
+        [MaxLength(4)]
+        public int? Year { get; set; }
+    }
+}
