40b89fcb3ba5f70619f8549b4d1126e235048323
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-11-26 15:44:30 +0100

    Moved basic repeating calls to base Service class

--- BookStore.Services/AuthorService.cs
+++ BookStore.Services/AuthorService.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using SystemValidationContext = System.ComponentModel.DataAnnotations.ValidationContext;
-using System.Text;
 using System.Threading.Tasks;
 using AutoMapper;
 using BookStore.Data.Repositories.Contracts;
@@ -14,7 +11,7 @@ using Microsoft.AspNetCore.JsonPatch;
 
 namespace BookStore.Services
 {
-    public class AuthorService : IAuthorService
+    public class AuthorService : ServiceBase, IAuthorService
     {
         public AuthorService(IAuthorRepository authorRepository, IMapper mapper)
         {
@@ -25,63 +22,13 @@ namespace BookStore.Services
         private readonly IAuthorRepository _authorRepository;
         private readonly IMapper _mapper;
 
-        public async Task<IReadOnlyList<AuthorDto>> GetAllAuthors()
-        {
-            var authors = await _authorRepository.GetAllAsync();
-            return _mapper.Map<IReadOnlyList<AuthorDto>>(authors);
-        }
-
-        public async Task<AuthorDto> GetAuthor(Guid id)
-        {
-            ValidateId(id);
-            var author = await _authorRepository.GetByIdAsync(id);
-
-            return author == null ? null : _mapper.Map<AuthorDto>(author);
-        }
-
         public async Task<AuthorDto> CreateAuthor(AuthorCreationDto authorCreationDto)
         {
-            ValidateAuthor(authorCreationDto);
+            ValidateEntity(authorCreationDto);
 
             var author = _mapper.Map<Author>(authorCreationDto);
             await _authorRepository.CreateAuthorAsync(author);
-            var authorDto = _mapper.Map<AuthorDto>(author);
-            return authorDto;
-        }
-
-        public async Task UpdateAuthor(Guid id, AuthorUpdateDto authorUpdateDto)
-        {
-            ValidateId(id);
-            ValidateAuthor(authorUpdateDto);
-
-            var author = await _authorRepository.GetByIdAsync(id);
-
-            if (author == null)
-            {
-                throw new AuthorNotFoundException(id);
-            }
-
-            _mapper.Map(authorUpdateDto, author);
-            await _authorRepository.SaveAsync();
-        }
-
-        public async Task PatchAuthor(Guid id, JsonPatchDocument<AuthorUpdateDto> jsonPatchDocument)
-        {
-            ValidateId(id);
-            var author = await _authorRepository.GetByIdAsync(id);
-
-            if (author == null)
-            {
-                throw new AuthorNotFoundException(id);
-            }
-
-            var authorUpdateDto = _mapper.Map<AuthorUpdateDto>(author);
-            jsonPatchDocument.ApplyTo(authorUpdateDto);
-
-            ValidateAuthor(authorUpdateDto);
-
-            _mapper.Map(authorUpdateDto, author);
-            await _authorRepository.SaveAsync();
+            return _mapper.Map<AuthorDto>(author);
         }
 
         public async Task DeleteAuthor(Guid id)
@@ -98,35 +45,53 @@ namespace BookStore.Services
             await _authorRepository.DeleteAuthorAsync(id);
         }
 
-        private static void ValidateAuthor<T>(T author) where T : class
+        public async Task<IReadOnlyList<AuthorDto>> GetAllAuthors()
         {
-            if (author == null)
-            {
-                throw new ArgumentException(nameof(author));
-            }
-
-            var validationContext = new SystemValidationContext(author);
-            var validationResults = new List<ValidationResult>();
-
-            if (!Validator.TryValidateObject(author, validationContext, validationResults, true))
-            {
-                var builder = new StringBuilder();
-
-                foreach (var result in validationResults)
-                {
-                    builder.AppendLine($"{string.Join(',', result.MemberNames)} : {string.Join(',', result.ErrorMessage)}");
-                }
-
-                throw new ValidationException($"Author failed validation. {Environment.NewLine} {builder}");
-            }
+            var authors = await _authorRepository.GetAllAsync();
+            return _mapper.Map<IReadOnlyList<AuthorDto>>(authors);
         }
 
-        private static void ValidateId(Guid id)
+        public async Task<AuthorDto> GetAuthor(Guid id)
         {
-            if (id == Guid.Empty)
+            ValidateId(id);
+
+            var author = await _authorRepository.GetByIdAsync(id);
+            return author == null ? null : _mapper.Map<AuthorDto>(author);
+        }
+
+        public async Task PatchAuthor(Guid id, JsonPatchDocument<AuthorUpdateDto> jsonPatchDocument)
+        {
+            ValidateId(id);
+            var author = await _authorRepository.GetByIdAsync(id);
+
+            if (author == null)
             {
-                throw new ArgumentException("Empty id specified.", nameof(id));
+                throw new AuthorNotFoundException(id);
             }
+
+            var authorUpdateDto = _mapper.Map<AuthorUpdateDto>(author);
+            jsonPatchDocument.ApplyTo(authorUpdateDto);
+
+            ValidateEntity(authorUpdateDto);
+
+            _mapper.Map(authorUpdateDto, author);
+            await _authorRepository.SaveAsync();
+        }
+
+        public async Task UpdateAuthor(Guid id, AuthorUpdateDto authorUpdateDto)
+        {
+            ValidateId(id);
+            ValidateEntity(authorUpdateDto);
+
+            var author = await _authorRepository.GetByIdAsync(id);
+
+            if (author == null)
+            {
+                throw new AuthorNotFoundException(id);
+            }
+
+            _mapper.Map(authorUpdateDto, author);
+            await _authorRepository.SaveAsync();
         }
     }
 }
