9a95874f7f94280f28a5cc4c6c19ee5d1370586b
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-11-26 15:27:53 +0100

    Added DTO for book creation

---
+++ BookStore.Domain/Entities/Dto/BookCreationDto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Domain.Entities.Dto
+{
+    public class BookCreationDto
+    {
+        [Required]
+        public Guid AuthorId { get; set; }
+        public string ImageUrl { get; set; }
+
+        [StringLength(50)]
+        [Required]
+        public string Isbn { get; set; }
+
+        [Required]
+        public double? Price { get; set; }
+        public string Summary { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Range(1700, 9999)]
+        public int? Year { get; set; }
+    }
+}
