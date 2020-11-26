196b556e7cbcc995ded11d4dc951c9d14b317e5d
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-11-26 09:47:51 +0100

    Added BookNotFoundException

---
+++ BookStore.Domain/Exceptions/BookNotFoundException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace BookStore.Domain.Exceptions
+{
+    public class BookNotFoundException : Exception
+    {
+        public BookNotFoundException()
+        {
+        }
+
+        public BookNotFoundException(Guid id)
+            : base($"Book with id: {id} not found.")
+        {
+        }
+
+        public BookNotFoundException(string message)
+            : base(message)
+        {
+        }
+
+        protected BookNotFoundException(SerializationInfo info, StreamingContext context)
+            : base(info, context)
+        {
+        }
+    }
+}
