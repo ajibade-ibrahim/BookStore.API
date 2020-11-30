3f668d595eb9d682f70224633b40fcdafbf5ef05
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-11-30 16:37:30 +0100

    Added Jwt Configuration class

---
+++ BookStore.API/Models/JwtConfiguration.cs
@@ -0,0 +1,14 @@
+﻿namespace BookStore.API.Models
+{
+    public interface IJwtConfiguration
+    {
+        string Issuer { get; set; }
+        string Key { get; set; }
+    }
+
+    public class JwtConfiguration : IJwtConfiguration
+    {
+        public string Issuer { get; set; }
+        public string Key { get; set; }
+    }
+}
