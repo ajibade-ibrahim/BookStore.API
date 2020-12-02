82db2c309bdb190452fb2f810a7ba7a81f56680a
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-02 15:28:32 +0100

    Implemented Login and Register actions

--- BookStore.API/Controller/AccountsController.cs
+++ BookStore.API/Controller/AccountsController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 using BookStore.API.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
@@ -15,22 +16,95 @@ namespace BookStore.API.Controller
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AccountsController : ControllerBase
+    public class AccountsController : BookStoreControllerBase
     {
         public AccountsController(
             UserManager<IdentityUser> userManager,
             SignInManager<IdentityUser> signInManager,
-            JwtConfiguration jwtConfiguration)
+            JwtConfiguration jwtConfiguration,
+            ILogger<AccountsController> logger)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _jwtConfiguration = jwtConfiguration;
+            _logger = logger;
         }
 
         private readonly JwtConfiguration _jwtConfiguration;
+        private readonly ILogger<AccountsController> _logger;
 
         private readonly SignInManager<IdentityUser> _signInManager;
         private readonly UserManager<IdentityUser> _userManager;
+
+        [Route("/login")]
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] UserLoginModel userLoginModel)
+        {
+            if (userLoginModel == null)
+            {
+                return BadRequest();
+            }
+
+            var result = await _signInManager.PasswordSignInAsync(userLoginModel.Username, userLoginModel.Password, true, false);
+
+            if (result?.Succeeded == true)
+            {
+                var token = await GenerateJwtToken(await _userManager.FindByNameAsync(userLoginModel.Username));
+                return Ok(token);
+            }
+
+            return Unauthorized(userLoginModel);
+        }
+
+        /// <summary>
+        ///   Registers a user with the given credentials.
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        [Route("/register")]
+        [HttpPost]
+        public async Task<IActionResult> Register([FromBody] UserRegistrationModel model)
+        {
+            if (model == null)
+            {
+                return BadRequest();
+            }
+
+            var result = await _userManager.CreateAsync(
+                new IdentityUser
+                {
+                    UserName = model.Username,
+                    Email = model.Username
+                });
+
+            if (result?.Succeeded == true)
+            {
+                _logger.LogError($"Registration succeeded for user: {model.Username}");
+                var user = await _userManager.FindByNameAsync(model.Username);
+
+                if (user != null)
+                {
+                    var passwordResult = await _userManager.AddPasswordAsync(user, model.Password);
+
+                    if (passwordResult?.Succeeded == true)
+                    {
+                        return Ok();
+                    }
+
+                    _logger.LogError($"Password addition failed for user: {model.Username}");
+                    _logger.LogError($"Deleting user: {model.Username}");
+                    await _userManager.DeleteAsync(user);
+
+                    var errors = passwordResult?.Errors.Select(x => x.Description) ?? new List<string>();
+                    return InternalServerErrorResult(
+                        $"Registration failed for user: {model.Username} {Environment.NewLine} {string.Join(';', errors)}");
+                }
+            }
+
+            result?.Errors.ToList().ForEach(error => _logger.LogError($"{error.Code}: {error.Description}"));
+
+            return InternalServerErrorResult($"Registration failed for user: {model.Username}");
+        }
 
         private async Task<string> GenerateJwtToken(IdentityUser user)
         {
@@ -57,31 +131,6 @@ namespace BookStore.API.Controller
                 credentials);
 
             return new JwtSecurityTokenHandler().WriteToken(token);
-        }
-
-        /// <summary>
-        /// Retrieves an authentication token.
-        /// </summary>
-        /// <param name="userInfo"></param>
-        /// <returns></returns>
-        [Route("/login")]
-        [HttpPost]
-        public async Task<IActionResult> Login([FromBody] UserInfo userInfo)
-        {
-            if (userInfo == null)
-            {
-                return BadRequest();
-            }
-
-            var result = await _signInManager.PasswordSignInAsync(userInfo.Username, userInfo.Password, true, false);
-
-            if (result?.Succeeded == true)
-            {
-                var token = await GenerateJwtToken(await _userManager.FindByNameAsync(userInfo.Username));
-                return Ok(token);
-            }
-
-            return Unauthorized(userInfo);
         }
     }
 }
--- BookStore.API/Models/UserInfo.cs
+++ BookStore.API/Models/UserInfo.cs
@@ -2,13 +2,28 @@
 
 namespace BookStore.API.Models
 {
-    public class UserInfo
+    public class UserLoginModel
     {
         [Required]
         [DataType(DataType.Password)]
         public string Password { get; set; }
 
         [Required]
+        public string Username { get; set; }
+    }
+
+    public class UserRegistrationModel
+    {
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Username { get; set; }
     }
 }
