fe1b4317b4a03de453389cbd209eb3dc55eb888a
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-02 15:29:59 +0100

    Implemented a Url generation service

---
+++ BookStore.BlazorServer/Services/UrlService.cs
@@ -0,0 +1,32 @@
+﻿namespace BookStore.BlazorServer.Services
+{
+    public class UrlService
+    {
+        public string AuthorsEndpoint
+        {
+            get => $"{BaseUrl}/Authors";
+        }
+
+        public string BaseUrl { get; set; }
+
+        public string BooksEndpoint
+        {
+            get => $"{BaseUrl}/Books";
+        }
+
+        public string LoginEndpoint
+        {
+            get => $"{BaseUrl}/login";
+        }
+
+        public string RegistrationEndpoint
+        {
+            get => $"{BaseUrl}/register";
+        }
+
+        public UrlService(string baseUrl)
+        {
+            BaseUrl = baseUrl;
+        }
+    }
+}
