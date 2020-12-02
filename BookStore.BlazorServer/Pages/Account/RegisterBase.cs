73f775a951fe241170d509c26e874fe6dfb4dd08
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-02 15:31:28 +0100

    Created Register component and implemented registration operation

---
+++ BookStore.BlazorServer/Pages/Account/RegisterBase.cs
@@ -0,0 +1,34 @@
+﻿using System.Threading.Tasks;
+using BookStore.BlazorServer.Models;
+using BookStore.BlazorServer.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BookStore.BlazorServer.Pages.Account
+{
+    public class RegisterBase : ComponentBase
+    {
+        [Inject]
+        public AccountService AccountService { get; set; }
+
+        public RegistrationModel Model { get; set; } = new RegistrationModel();
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        public ServiceResponse Response { get; set; } = new ServiceResponse();
+
+        public bool ShowError
+        {
+            get => !string.IsNullOrWhiteSpace(Response?.Message) && !Response.Succeeded;
+        }
+
+        public async Task Register()
+        {
+            Response = await AccountService.Register(Model);
+
+            if (Response.Succeeded)
+            {
+                NavigationManager.NavigateTo("/");
+            }
+        }
+    }
+}
--- BookStore.BlazorServer/Startup.cs
+++ BookStore.BlazorServer/Startup.cs
@@ -1,15 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Net.Http;
+using BookStore.BlazorServer.Data;
+using BookStore.BlazorServer.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BookStore.BlazorServer.Data;
 
 namespace BookStore.BlazorServer
 {
@@ -22,15 +19,6 @@ namespace BookStore.BlazorServer
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
-            services.AddSingleton<WeatherForecastService>();
-        }
-
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -41,6 +29,7 @@ namespace BookStore.BlazorServer
             else
             {
                 app.UseExceptionHandler("/Error");
+
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
@@ -50,11 +39,24 @@ namespace BookStore.BlazorServer
 
             app.UseRouting();
 
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
-            });
+            app.UseEndpoints(
+                endpoints =>
+                {
+                    endpoints.MapBlazorHub();
+                    endpoints.MapFallbackToPage("/_Host");
+                });
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            services.AddSingleton<WeatherForecastService>();
+            services.AddScoped<HttpClient>();
+            services.AddScoped<AccountService>();
+            services.AddScoped(provider => new UrlService(Configuration["ApiBaseUrl"]));
         }
     }
 }
