a39b4ce2034bd855f752cfa66a2cd82aa4e90c94
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-04 08:59:31 +0100

    Added login component

---
+++ BookStore.BlazorServer/Pages/Account/LoginBase.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using BookStore.BlazorServer.Models;
+using BookStore.BlazorServer.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace BookStore.BlazorServer.Pages.Account
+{
+    public class LoginBase : ComponentBase
+    {
+        public LoginModel Model { get; set; } = new LoginModel();
+        public ServiceResponse Response { get; set; } = new ServiceResponse();
+
+        [Inject]
+        public AccountService AccountService { get; set; }
+
+        [Inject]
+        public NavigationManager Navigator { get; set; }
+
+        public bool HasErrorMessage
+        {
+            get => !string.IsNullOrWhiteSpace(Response?.Message) && !Response.Succeeded;
+        }
+
+        public async Task Login()
+        {
+            try
+            {
+                Response = await AccountService.Login(Model);
+
+                if (Response.Succeeded)
+                {
+                    Navigator.NavigateTo("/");
+                }
+            }
+            catch (Exception exception)
+            {
+                Response.Succeeded = false;
+                Response.Message = exception.Message;
+            }
+        }
+    }
+}
