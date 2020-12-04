033ccdac96b59eaec93bef85bb8b8e8c79541bb2
ajibade-ibrahim <ajibade.ibrahim@aurea.com>
2020-12-04 08:58:37 +0100

    Added HttpClientHandler to insert bearer token

---
+++ BookStore.BlazorServer/HttpHandlers/BearerTokenHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading;
+using System.Threading.Tasks;
+using Blazored.LocalStorage;
+using BookStore.BlazorServer.Models;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore.BlazorServer.HttpHandlers
+{
+    public class BearerTokenHandler : DelegatingHandler
+    {
+        public BearerTokenHandler(ILocalStorageService localStorage, ILogger<BearerTokenHandler> logger)
+        {
+            _localStorage = localStorage;
+            _logger = logger;
+        }
+
+        private readonly ILocalStorageService _localStorage;
+        private readonly ILogger<BearerTokenHandler> _logger;
+
+        protected override async Task<HttpResponseMessage> SendAsync(
+            HttpRequestMessage request,
+            CancellationToken cancellationToken)
+        {
+            try
+            {
+                var authToken = await _localStorage.GetItemAsync<AuthToken>("AuthToken");
+
+                if (!string.IsNullOrWhiteSpace(authToken?.Token))
+                {
+                    request.Headers.Authorization = new AuthenticationHeaderValue("bearer", authToken.Token);
+                }
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError($"Error occurred: {exception.Message}");
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+    }
+}
